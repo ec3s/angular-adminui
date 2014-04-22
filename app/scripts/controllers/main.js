39b76147db8f4743dcaafcc1388c66af25a1370f
Fengming Sun <s@sfmblog.cn>
2014-04-23 00:19:09 +0800

    fix

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -712,3 +712,78 @@ var ModalInstanceCtrl = function($scope, $modalInstance, items) {
 adminuiApp.controller('ModalInstanceCtrl', [
   '$scope', '$modalInstance', 'items'
 ]);
+var DatepickerDemoCtrl = function($scope, $timeout) {
+  $scope.today = function() {
+    $scope.dt = new Date();
+  };
+  $scope.today();
+
+  $scope.showWeeks = true;
+  $scope.toggleWeeks = function() {
+    $scope.showWeeks = ! $scope.showWeeks;
+  };
+
+  $scope.clear = function() {
+    $scope.dt = null;
+  };
+
+  // Disable weekend selection
+  $scope.disabled = function(date, mode) {
+    return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
+  };
+
+  $scope.toggleMin = function() {
+    $scope.minDate = ($scope.minDate) ? null : new Date();
+  };
+  $scope.toggleMin();
+
+  $scope.open = function() {
+    $timeout(function() {
+      $scope.opened = true;
+    });
+  };
+
+  $scope.dateOptions = {
+    'year-format': "'yy'",
+    'starting-day': 1
+  };
+
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'shortDate'];
+  $scope.format = $scope.formats[0];
+};
+
+adminuiApp.controller('DatepickerDemoCtrl', [
+  '$scope', '$timeout', DatepickerDemoCtrl
+]);
+var TimepickerDemoCtrl = function($scope) {
+  $scope.mytime = new Date();
+
+  $scope.hstep = 1;
+  $scope.mstep = 15;
+
+  $scope.options = {
+    hstep: [1, 2, 3],
+    mstep: [1, 5, 10, 15, 25, 30]
+  };
+
+  $scope.ismeridian = true;
+  $scope.toggleMode = function() {
+    $scope.ismeridian = ! $scope.ismeridian;
+  };
+
+  $scope.update = function() {
+    var d = new Date();
+    d.setHours(14);
+    d.setMinutes(0);
+    $scope.mytime = d;
+  };
+
+  $scope.changed = function() {
+    console.log('Time changed to: ' + $scope.mytime);
+  };
+
+  $scope.clear = function() {
+    $scope.mytime = null;
+  };
+};
+adminuiApp.controller('TimepickerDemoCtrl', ['$scope', TimepickerDemoCtrl]);
