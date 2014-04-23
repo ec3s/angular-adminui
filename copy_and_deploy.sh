e308f86ea624a834d9df112be83d890e5ea0cddd
Fengming Sun <s@sfmblog.cn>
2014-04-23 19:37:21 +0800

    fix

--- copy_and_deploy.sh
+++ copy_and_deploy.sh
@@ -10,7 +10,9 @@ cp -r ./dist/* ../angular-adminui-page/
 git commit -a
 git push
 commit_version=`git show |sed 'q'|awk '{print $2}'`
+git -C ../angular-adminui-bower add -A
 git -C ../angular-adminui-bower commit -a -m 'release by ec3s/angular-adminui@'$commit_version
 git -C ../angular-adminui-bower push
+git -C ../angular-adminui-page add -A
 git -C ../angular-adminui-page commit -a -m 'release by ec3s/angular-adminui@'$commit_version
 git -C ../angular-adminui-page push
