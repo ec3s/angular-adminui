562a1bbcc51bea9530a4e6c94093dfb6f55ba9a7
dengzibiao <dengzibiao@ec3s.com>
2014-07-09 16:56:05 +0800

    time-line指令模板路径改

--- app/scripts/directives/adminui-time-line.js
+++ app/scripts/directives/adminui-time-line.js
@@ -5,7 +5,7 @@
       'restrict': 'EA',
       replace: true,
       require: '^ngModel',
-      templateUrl:'../templates/adminui-time-line.html'
+      templateUrl:'templates/adminui-time-line.html'
     };
   };
   var AdminuiTimeLine =
