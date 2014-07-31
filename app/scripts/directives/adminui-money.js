98f8c97b31a57d4da42b5b17246fb94b9834e3ea
dengzibiao <dengzibiao@ec3s.com>
2014-07-31 15:09:19 +0800

    adminui的money组件加入最大值参数

--- app/scripts/directives/adminui-money.js
+++ app/scripts/directives/adminui-money.js
@@ -5,11 +5,19 @@
       restrict: 'A',
       require: 'ngModel',
       link: function(scope, elem, attrs, ctrl) {
-        elem.maskMoney({prefix:'￥',allowZero:true,thousands:''});
+        var maxMoney;
+        elem.maskMoney({prefix:'￥',allowZero:true,thousands:','});
+        var initialize = function(value) {
+          maxMoney = Number(value);
+        };
         var parser = function () {
+          if (maxMoney && elem.maskMoney('unmasked')[0] > maxMoney) {
+            elem.maskMoney('mask', maxMoney);
+          }
           ctrl.$viewValue = elem.maskMoney('unmasked')[0];
           return ctrl.$viewValue;
         };
+        attrs.$observe('adminuiMoney', initialize);
         ctrl.$parsers.push(parser);
       }
     }
