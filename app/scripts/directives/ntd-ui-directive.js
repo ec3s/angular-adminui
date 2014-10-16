6ee0517ead21c1bb1b67c3685b2278080e792091
Fengming Sun <s@sfmblog.cn>
2014-10-16 14:17:50 +0800

    fix

--- app/scripts/directives/ntd-ui-directive.js
+++ app/scripts/directives/ntd-ui-directive.js
@@ -1,5 +1,24 @@
 'use strict';
-angular.module('ntd.config', []).value('$ntdConfig', {});
+window.adminConfigApp = angular.module('ntd.config', []).value('$ntdConfig', {});
 angular.module('ntd.directives',
   ['ntd.config', 'ngSanitize', 'angular-echarts', 'ng.shims.placeholder'
   ]);
+
+var httpInterceptorFn = function($httpProvider, $provide) {
+    $provide.factory("AdminuiHttpInterceptor", function() {
+        return {
+            request: function(config) {
+                if (config.method == 'GET' && !config.hasOwnProperty('cache')) {
+                    if (!config.hasOwnProperty('params')) {
+                      config.params = {};
+                  }
+                  var date = new Date();
+                  config.params['_hash_'] = date.getTime().toString();
+              }
+              return config;
+          }
+      };
+    });
+};
+
+window.adminConfigApp.config(['$httpProvider', '$provide', httpInterceptorFn]);
