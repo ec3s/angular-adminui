a8ee9801138a1e13aef95a73a35777e322539e96
dengzibiao <dengzibiao@ec3s.com>
2014-10-16 16:06:05 +0800

    fix notice

--- app/scripts/directives/ntd-notice.js
+++ app/scripts/directives/ntd-notice.js
@@ -1,80 +1,44 @@
 /* notcie */
-(function() {
+(function(ng) {
   'use strict';
-  var msgObj = {
-    'info': 'alert-info',
-    'error': 'alert-danger',
-    'success': 'alert-success',
-    'warning': 'alert-warning'
-  };
 
-  var alertModel = function(message) {
-    return '<div class="alert ' +
-      msgObj[message.state] + '">' +
-      '<strong>' + message.info + '</strong>' +
-      '<button type="button" class="close">×</button>' +
-      '</div>';
-  };
-
-  function noticeDirective($rootScope, $location, $timeout) {
+  function noticeDirective($rootScope, $location, $timeout, noticeService) {
     return {
       restrict: 'EAC',
       replace: false,
       transclude: false,
-      link: function(scope, element, attr) {
-        var html_fragement = '';
-        var flag = false;
-        var buildHtml = function(msg) {
-          if (angular.isArray(msg)) {
-            angular.forEach(msg, function(item) {
-              html_fragement += alertModel(item);
-            });
-          } else {
-            html_fragement += alertModel(msg);
-          }
-        };
-        var appendAndAddCloseListener = function() {
-          element.append(html_fragement);
-          element.next().css('padding-top', element.height());
-          $('.close', element).bind('click', function() {
-            $(this).parent('.alert').fadeOut(function() {
-              $(this).remove();
-              element.next().css('padding-top', element.height());
-            });
-          });
-          html_fragement = '';
-        };
+      templateUrl: 'templates/adminui-notice.html',
+      link: function() {
+        var tempMessage;
 
         $rootScope.$on('event:notification', function(event, message) {
-          flag = true;
-          buildHtml(message);
-          appendAndAddCloseListener();
-
           if (message.redirect_url) {
             $timeout(function() {
               $location.path(message.redirect_url);
+              tempMessage = message;
             }, 1500);
+          } else {
+            noticeService.addAlert(message);
           }
         });
         $rootScope.$on('event:flashMessageEvent', function(event, msg) {
-          flag = false;
-          buildHtml(msg);
+          tempMessage = msg;
         });
 
+        $rootScope.$on('$routeChangeStart', function() {
+          $rootScope.alerts = [];
+        });
         $rootScope.$on('$routeChangeSuccess', function() {
-          if (flag) {
-            element.empty();
-            element.next().css('padding-top', 0);
-          } else {
-            element.empty();
-            appendAndAddCloseListener();
+          if (tempMessage) {
+            noticeService.addAlert(ng.copy(tempMessage));
           }
+          tempMessage = null;
         });
       }
     };
   }
-  angular.module('ntd.directives').directive('notice', [
-    '$rootScope', '$location', '$timeout',
-    noticeDirective
-  ]);
-}());
+  ng.module('ntd.directives').directive('notice', [
+      '$rootScope', '$location', '$timeout', 'noticeService',
+      noticeDirective
+    ]);
+}(angular));
--- app/scripts/directives/ntd-ui-directive.js
+++ app/scripts/directives/ntd-ui-directive.js
@@ -1,5 +1,13 @@
 'use strict';
 angular.module('ntd.config', []).value('$ntdConfig', {});
-angular.module('ntd.directives',
-  ['ntd.config', 'ngSanitize', 'angular-echarts', 'ng.shims.placeholder'
-  ]);
+var ntdDirective = angular.module('ntd.directives', ['ntd.config', 'ngSanitize',
+  'ngAnimate']);
+ntdDirective.run(['$rootScope', '$animate',
+  function($rootScope, $animate) {
+    $rootScope.$on('$routeChangeStart', function() {
+      $animate.enabled(false);
+    });
+    $rootScope.$on('$routeChangeSuccess', function() {
+      $animate.enabled(true);
+    });
+  }]);
