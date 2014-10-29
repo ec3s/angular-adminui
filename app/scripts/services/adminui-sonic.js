4f6fe5bf42e4dd47d70ce9b729ba81e2e45da6d9
dengzibiao <dengzibiao@ec3s.com>
2014-10-29 11:30:22 +0800

    fix

--- app/scripts/services/adminui-sonic.js
+++ app/scripts/services/adminui-sonic.js
@@ -20,7 +20,7 @@
       fillColor: '#1196EE',
       trailLength: 0.01,
       pointDistance: 0.01,
-      fps: 20,
+      fps: 30,
       step: function(point, index, frame) {
         var ctx = this._;
         ctx.fillText(0|frame*this.fps, 1, 99);
